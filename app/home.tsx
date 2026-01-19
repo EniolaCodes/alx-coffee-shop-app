f800ac5e1f22ced946f9377ff4ab93876f54d40b
EniolaCodes <adebimpeniola@gmail.com>
2026-01-19 15:34:08 +0100

    refactor the Home screen into a cleaner loop using a data array

--- app/home.tsx
+++ app/home.tsx
@@ -23,6 +23,57 @@ import {
 
 const categories = ["All Coffee", "Machiato", "Latte", "Americano"];
 
+const COFFEE_DATA = [
+  {
+    id: "1",
+    name: "Caffe Mocha",
+    description: "Deep Foam",
+    price: "4.53",
+    rating: 4.8,
+    image: require("../assets/images/coffee-1.png"),
+  },
+  {
+    id: "2",
+    name: "Flat White",
+    description: "Espresso",
+    price: "3.53",
+    rating: 4.8,
+    image: require("../assets/images/coffee-2.png"),
+  },
+  {
+    id: "3",
+    name: "Turkish Coffee",
+    description: "Strong Aroma",
+    price: "5.00",
+    rating: 4.9,
+    image: require("../assets/images/coffee-3.png"),
+  },
+  {
+    id: "4",
+    name: "Espresso",
+    description: "Strong Aroma",
+    price: "5.00",
+    rating: 4.9,
+    image: require("../assets/images/coffee-4.png"),
+  },
+  {
+    id: "5",
+    name: "Iced Latte",
+    description: "Strong Aroma",
+    price: "5.00",
+    rating: 4.9,
+    image: require("../assets/images/coffee-5.png"),
+  },
+  {
+    id: "6",
+    name: "Cappuccino",
+    description: "Deep Foam",
+    price: "5.00",
+    rating: 4.9,
+    image: require("../assets/images/coffee-6.png"),
+  },
+];
+
 // Define hex codes to match your NativeWind theme for the icons
 const COLORS = {
   coffee: "#C67C4E",
@@ -72,8 +123,6 @@ export default function HomeScreen() {
         </View>
 
         <View className=" -mt-12 px-6">
-          {" "}
-          {/* Negative margin pulls it up */}
           <Image
             source={require("../assets/images/Banner.png")}
             className="w-full h-32 rounded-xl"
@@ -109,130 +158,31 @@ export default function HomeScreen() {
 
         {/* Coffee Grid */}
         <View className="flex-row flex-wrap justify-between px-6 mt-6 pb-32">
-          {/* Example Cards */}
-          <CoffeeCard
-            name="Caffe Mocha"
-            description="Deep Foam"
-            price="4.53"
-            rating={4.8}
-            image={require("../assets/images/coffee-1.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "1",
-                  name: "Caffe Mocha",
-                  price: "4.53",
-                  description: "Deep Foam",
-                  rating: "4.8",
-                  image: String(require("../assets/images/coffee-1.png")),
-                },
-              })
-            }
-          />
-          <CoffeeCard
-            name="Flat White"
-            description="Espresso"
-            price="3.53"
-            rating={4.8}
-            image={require("../assets/images/coffee-2.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "2",
-                  name: "Flat White",
-                  price: "3.53",
-                  description: "Espresso",
-                  rating: "4.8",
-                  image: String(require("../assets/images/coffee-2.png")),
-                },
-              })
-            }
-          />
-          <CoffeeCard
-            name="Turkish Coffee"
-            description="Strong Aroma"
-            price="5.00"
-            rating={4.9}
-            image={require("../assets/images/coffee-3.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "3",
-                  name: "Turkish Coffee",
-                  price: "5.00",
-                  description: "Strong Aroma",
-                  rating: "4.9",
-                  image: String(require("../assets/images/coffee-3.png")),
-                },
-              })
-            }
-          />
-          <CoffeeCard
-            name="Turkish Coffee"
-            description="Strong Aroma"
-            price="5.00"
-            rating={4.9}
-            image={require("../assets/images/coffee-4.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "4",
-                  name: "Caffe Mocha",
-                  price: "5.00",
-                  description: "Strong Aroma",
-                  rating: "4.9",
-                  image: String(require("../assets/images/coffee-4.png")),
-                },
-              })
-            }
-          />
-          <CoffeeCard
-            name="Turkish Coffee"
-            description="Strong Aroma"
-            price="5.00"
-            rating={4.9}
-            image={require("../assets/images/coffee-5.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "5",
-                  name: "Turkish Coffee",
-                  price: "5.00",
-                  description: "Strong Aroma",
-                  rating: "4.9",
-                  image: String(require("../assets/images/coffee-5.png")),
-                },
-              })
-            }
-          />
-          <CoffeeCard
-            name="Turkish Coffee"
-            description="Strong Aroma"
-            price="5.00"
-            rating={4.9}
-            image={require("../assets/images/coffee-6.png")}
-            onPress={() =>
-              router.push({
-                pathname: "/details/[id]",
-                params: {
-                  id: "6",
-                  name: "Caffe Mocha",
-                  price: "5.00",
-                  description: "Deep Foam",
-                  rating: "4.9",
-                  image: String(require("../assets/images/coffee-6.png")),
-                },
-              })
-            }
-          />
+          {COFFEE_DATA.map((item) => (
+            <CoffeeCard
+              key={item.id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              rating={item.rating}
+              image={item.image}
+              onPress={() =>
+                router.push({
+                  pathname: "/details/[id]",
+                  params: {
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    description: item.description,
+                    rating: item.rating.toString(),
+                    image: String(item.image),
+                  },
+                })
+              }
+            />
+          ))}
         </View>
       </ScrollView>
-
       <View className="absolute bottom-0 left-0 right-0 bg-white flex-row justify-around pt-4 pb-8 border-t border-gray-100 rounded-t-[30px] shadow-2xl">
         <TabItem
           icon={HouseIcon}
